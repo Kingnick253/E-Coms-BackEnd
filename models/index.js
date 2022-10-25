b9e85e75ec3a799fd0d6704b11144eca7b208156
Kingnick253 <nicholaswebb2016@gmail.com>
2022-10-25 13:31:32 -0700

    Finished Product.js model and add some comments for my route placement

--- models/index.js
+++ models/index.js
@@ -5,6 +5,10 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+  // Product.belongsTo(Category, {
+  //   deleteOn: 'CASCADE',
+  //   foriegnKey:
+  // });
 
 // Categories have many Products
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,36 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+
+  //Category.findAll()
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  // be sure to include its associated Products!!
+
+  // Category.findbyPK(req.parmas.id)
 });
 
 router.post('/', (req, res) => {
   // create a new category
+
+  //Category.create(req.body.category_name)
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  /** 
+   * // Category.update(req.body.id, {
+   *  where { id: req.parmas.id }
+   * })
+   * */
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+
+  //  Category.destory
 });
 
 module.exports = router;
