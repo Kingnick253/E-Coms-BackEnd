d3cb563b528987dcb32c1ce52fdff12fcce680db
Kingnick253 <nicholaswebb2016@gmail.com>
2022-10-25 13:43:08 -0700

    finished Index.js for the all of my models

--- models/index.js
+++ models/index.js
@@ -4,18 +4,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-  // Product.belongsTo(Category, {
-  //   deleteOn: 'CASCADE',
-  //   foriegnKey:
-  // });
+// Products.belongsTo Category
+  Product.belongsTo(Category, {
+    deleteOn: 'CASCADE',
+    foriegnKey: 'catergory_id'
+  });
 
 // Categories have many Products
-
+   Category.hasMany(Product,{
+    foreignKey: 'catergory_id'
+   });
 // Products belongToMany Tags (through ProductTag)
-
+   Product.belongsToMany(ProductTag, {
+    deleteOn: 'CASCADE',
+    foreignKey: 'product_id'
+   });
 // Tags belongToMany Products (through ProductTag)
-
+   Tag.belongsToMany(Product, {
+    deleteOn: "CASCADE",
+    foreignKey:'tag_id'
+   });
 module.exports = {
   Product,
   Category,
